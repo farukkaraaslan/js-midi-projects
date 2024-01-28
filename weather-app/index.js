0c0c49de8c2479281dd2e253652f10f1d9c75d71
farukkaraaslan <faruk.jsga.edu.tr>
2024-01-28 16:56:16 +0300

    uı tasarım işlem tamamlandı.

--- weather-app/index.js
+++ weather-app/index.js
@@ -25,6 +25,7 @@ async function getWeatherData(cityValue){
         console.log(data)
         const temperature = Math.round(data.main.temp)
         const description = data.weather[0].description
+        const city = data.name
         const icon = data.weather[0].icon
         console.log(icon    )
         const deatil = [
@@ -36,9 +37,10 @@ async function getWeatherData(cityValue){
 
         weatherDataElement.querySelector(".icon").innerHTML=`<img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon">`
         weatherDataElement.querySelector(".temperature").textContent = `${temperature}°C`
+        weatherDataElement.querySelector(".city h1").textContent = `${city}`
         weatherDataElement.querySelector(".description").textContent = `${description}`
         weatherDataElement.querySelector(".details").innerHTML = deatil.map((detail) => `<div>${detail}</div>`).join("")
     } catch (error) {
-        
+       console.log("şehir bulunamadı")
     }
 }
